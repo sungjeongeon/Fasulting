d0ad84e1e74779d99d46b931d57959b98d098b8e
mirim <mrmy@kakao.com>
2023-01-20 14:36:53 +0900

    Feat: Hospital Detail Page Layout 병원 상세 페이지 레이아웃 중간 커밋

---
+++ fasulting/src/components/Detail/SimpleInfo.js
@@ -0,0 +1,73 @@
+import React, { useState } from "react";
+
+import styles from "./SimpleInfo.module.css";
+import LocationOnIcon from "@mui/icons-material/LocationOn";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import TagIcon from "@mui/icons-material/Tag";
+
+function SimpleInfo() {
+  const ps_name = "더성형외과의원";
+  const ps_address = "부산 강서구 녹산산단 335로 7, 송삼빌딩 1층";
+  const sub_category = [
+    "쌍커풀",
+    "트임시술",
+    "지방흡입",
+    "콧볼축소",
+    "코끝",
+    "입꼬리",
+    "보톡스",
+    "보톡스",
+    "보톡스",
+    "보톡스",
+    "보톡스",
+    "보톡스",
+  ];
+  // liked(좋아요 상태) t/f
+  const [liked, setLiked] = useState(false);
+  const onClick = (e) => {
+    setLiked((current) => !current);
+    console.log(e.target.value);
+  };
+  return (
+    <div>
+      {/* 프로필 사진 */}
+      <div className={styles.profileImg}></div>
+      <div className={styles.nameAndLikeDiv}>
+        {/* 이름 */}
+        <p className={styles.name}>{ps_name}</p>
+        {/* 하트(좋아요/취소) */}
+        {/* [liked] t/f 에 따라 빨강/회색 */}
+        {liked ? (
+          <FavoriteIcon
+            fontSize="large"
+            sx={{ color: "#e64c3c" }}
+            onClick={onClick}
+          />
+        ) : (
+          <FavoriteIcon
+            fontSize="large"
+            sx={{ color: "#d9d4cf" }}
+            onClick={onClick}
+          />
+        )}
+      </div>
+      {/* 주소 */}
+      <div className={styles.address}>
+        <LocationOnIcon />
+        <p>{ps_address}</p>
+      </div>
+      {/* 수술 분야 (세부) */}
+      <p>
+        {sub_category.map((sub, index) => {
+          return (
+            <button key={index} className={styles.subCategory}>
+              <TagIcon sx={{ fontSize: 12 }} /> {sub}
+            </button>
+          );
+        })}
+      </p>
+    </div>
+  );
+}
+
+export default SimpleInfo;
--- fasulting/src/pages/client/Detail.js
+++ fasulting/src/pages/client/Detail.js
@@ -1,29 +1,25 @@
-import { useParams } from "react-router-dom";
+// import { useParams } from "react-router-dom";
 import ReserveCard from "../../components/Card/ReserveCard";
-import { styled } from "@mui/material/styles";
+// import { styled } from "@mui/material/styles";
 import Box from "@mui/material/Box";
-import Paper from "@mui/material/Paper";
+// import Paper from "@mui/material/Paper";
 import Grid from "@mui/material/Unstable_Grid2";
+import BackgroundImage from "../../components/Detail/BackgroundImage";
+import SimpleInfo from "../../components/Detail/SimpleInfo";
+import HospitalInfo from "../../components/Detail/HospitalInfo";
 
 function Detail() {
-  const { id } = useParams();
+  // const { id } = useParams();
+  // 병원 id로 병원 정보 요청 - 응답 받아서 각 컴포넌트로 전달 (예정)
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <Grid container spacing={2}>
+      <Grid container spacing={0}>
         {/* 프로필(배경) 이미지 */}
-        <div
-          style={{
-            position: "absolute",
-            left: "0",
-            width: "100vw",
-            height: "18rem",
-            backgroundColor: "gray",
-            opacity: "50%",
-          }}
-        ></div>
-        <Grid xs={12} style={{ height: "18rem" }}></Grid>
+        <BackgroundImage />
+        <Grid xs={12} style={{ height: "15rem" }}></Grid>
         <Grid xs={9}>
-          <h1>Detail</h1>
+          <SimpleInfo />
+          <HospitalInfo />
         </Grid>
         <Grid xs={3}>
           <ReserveCard />
