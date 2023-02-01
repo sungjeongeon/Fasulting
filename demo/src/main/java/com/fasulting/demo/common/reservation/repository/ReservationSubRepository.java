c2b1aaf2685006ab5599416521f765545be65cc2
0613minjeong <0613minjeong@gmail.com>
2023-02-01 17:45:32 +0900

    Feat. reservationSubRepository 복합키 수정

--- demo/src/main/java/com/fasulting/demo/common/reservation/repository/ReservationSubRepository.java
+++ demo/src/main/java/com/fasulting/demo/common/reservation/repository/ReservationSubRepository.java
@@ -1,6 +1,7 @@
 package com.fasulting.demo.common.reservation.repository;
 
 import com.fasulting.demo.entity.ReservationSubEntity;
+import com.fasulting.demo.entity.compositeId.ReservationSubId;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -9,7 +10,7 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
-public interface ReservationSubRepository extends JpaRepository<ReservationSubEntity, Long> {
+public interface ReservationSubRepository extends JpaRepository<ReservationSubEntity, ReservationSubId> {
 
     @Query("SELECT rs.subCategory.name " + "FROM ReservationSubEntity rs " + "WHERE rs.reservation.seq = :reservationSeq")
     List<String> getSubCategoryByReservationSeq(@Param("reservationSeq") Long reservationSeq);
