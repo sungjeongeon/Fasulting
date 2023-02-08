c91563e8969fbb070ab067cc4305a244c4f8d33a
mirim <mrmy@kakao.com>
2023-02-08 14:53:38 +0900

    Feat: 상담자 미래 예약 카드 api요청 확인

--- fasulting/src/components/Card/ConsultingCardList.js
+++ fasulting/src/components/Card/ConsultingCardList.js
@@ -4,62 +4,8 @@ import styles from "./FavResCard.module.css";
 import axios from "axios";
 import { useEffect } from "react";
 import axiosAPi from "../../api/axiosApi";
-function ConsultingCardList() {
-  useEffect(() => {
-    axiosAPi.get("/reservation/post/1").then((res) => {
-      console.log(res);
-    });
-  }, []);
-  const consulting = [
-    {
-      reservationSeq: 1,
-      userSeq: 1,
-      psSeq: 1,
-      psName: "아이디 병원",
-      subCategoryName: ["쌍커풀", "눈매교정"],
-      year: 2023,
-      month: 2,
-      day: 8,
-      hour: 21, // 24시간
-      minute: 12,
-    },
-    {
-      reservationSeq: 2,
-      userSeq: 2,
-      psSeq: 2,
-      psName: "더페이스 병원",
-      subCategoryName: ["콧볼축소", "콧대성형", "안면윤곽"],
-      year: 2023,
-      month: 2,
-      day: 8,
-      hour: 21, // 24시간
-      minute: 12,
-    },
-    {
-      reservationSeq: 3,
-      userSeq: 3,
-      psSeq: 3,
-      psName: "더페이스 병원",
-      subCategoryName: ["콧볼축소", "콧대성형"],
-      year: 2023,
-      month: 2,
-      day: 10,
-      hour: 21, // 24시간
-      minute: 12,
-    },
-    {
-      reservationSeq: 4,
-      userSeq: 4,
-      psSeq: 4,
-      psName: "더페이스 병원",
-      subCategoryName: ["콧볼축소", "콧대성형"],
-      year: 2023,
-      month: 2,
-      day: 10,
-      hour: 21, // 24시간
-      minute: 12,
-    },
-  ];
+import { useState } from "react";
+function ConsultingCardList({ consulting }) {
   return (
     <div className={styles.margin}>
       <h2>진행중인 예약</h2>
