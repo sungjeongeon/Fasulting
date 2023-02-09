aa53539f2a8be66c98c4e2ef8f86ea2f1f32faf0
mirimy <mirimy97@gmail.com>
2023-02-10 03:08:36 +0900

    Feat: 병원 견적서 이미지 업로드 추가

--- fasulting/src/components/Modal/LeaveConsulting.js
+++ fasulting/src/components/Modal/LeaveConsulting.js
@@ -18,12 +18,54 @@ import Box from "@mui/material/Box";
 import styles from "./LeaveConsulting.module.css";
 import { useState } from "react";
 import { useNavigate } from "react-router";
+import axiosAPi from "../../api/axiosApi";
 
 export default function LeaveConsulting({ leaveSession }) {
   const navigate = useNavigate();
   const [open, setOpen] = React.useState(false);
+  const [img, setImg] = useState([]);
   const [content, setContent] = useState("");
   const [cost, setCost] = useState(0);
+  const subCategory = ["쌍커풀", "앞트임"];
+
+  const submitReport = async () => {
+    leaveSession();
+    // 소견서 제출 api ( + 상담 id )
+    // {
+    //   "reservationSeq" : "예약 seq",
+    //   "reportContent" : "상담 소견",
+    //   "reportEstimate" : "예상 견적",
+    //   "afterImg" : "after 사진 파일"
+    // }
+    const formData = new FormData();
+    formData.append("reservationSeq", 47);
+    formData.append("reportContent", content);
+    formData.append("reportEstimate", cost);
+    formData.append("afterImg", img);
+    try {
+      await axiosAPi.post("/ps-consulting/result", formData, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+          //Authorization: localStorage.getItem("access_token")
+        },
+        withCredentials: false,
+      });
+    } catch (e) {
+      // FormData의 key 확인
+      for (let key of formData.keys()) {
+        console.log(key);
+      }
+
+      // FormData의 value 확인
+      for (let value of formData.values()) {
+        console.log(value);
+      }
+      console.log(e);
+    }
+
+    // 예약 스케줄 페이지로 라우팅
+    // navigate("/myreservationho");
+  };
 
   const handleClickOpen = () => {
     setOpen(true);
@@ -34,23 +76,21 @@ export default function LeaveConsulting({ leaveSession }) {
     setOpen(false);
   };
 
+  // 이미지 업로드
+  //파일(이미지) 업로드
+  const onChange = (e) => {
+    const uploadimg = e.target.files[0];
+    setImg(uploadimg);
+    // const formData = new FormData();
+    // formData.append("file", img);
+  };
+
   const contentChanged = (e) => {
     setContent(e.target.value);
   };
   const costChanged = (e) => {
     setCost(e.target.value);
   };
-
-  const submitReport = () => {
-    leaveSession();
-    // 소견서 제출 api ( + 상담 id )
-    console.log(content);
-    console.log(cost);
-    // 예약 스케줄 페이지로 라우팅
-    navigate("/myreservationho");
-  };
-
-  const subCategory = ["쌍커풀", "앞트임"];
 
   return (
     <div>
@@ -64,6 +104,23 @@ export default function LeaveConsulting({ leaveSession }) {
               상담 내용을 기반으로 추천 시술, 견적 등 상담자에게 제공될 소견을
               작성해주세요.
             </DialogContentText>
+            {/* after 사진 첨부 */}
+            <DialogContentText sx={{ mt: 5 }}>
+              After 사진 파일첨부
+            </DialogContentText>
+            <div>
+              {/* <p className={styles.ptag}>반드시 정면이 나온 사진을 첨부해주세요</p> */}
+              <label htmlFor="inputfile">파일 선택</label>
+              <input
+                type="file"
+                id="inputfile"
+                accept="image/jpg,impge/png,image/jpeg,image/gif"
+                name="profile_img"
+                onChange={onChange}
+                style={{ display: "none" }}
+              ></input>
+              {img.name}
+            </div>
             <DialogContentText sx={{ mt: 5 }}>상담 항목</DialogContentText>
             <Box sx={{ display: "flex", flexWrap: "wrap", gap: 0.5 }}>
               {subCategory.map((value) => (
