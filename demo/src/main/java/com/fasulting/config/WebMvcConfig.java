a35e307b48196e89bbe97732ddc28e090ba942d2
good-da22 <rnxogml3000@gmail.com>
2023-02-09 17:07:50 +0900

    fix : login

--- demo/src/main/java/com/fasulting/config/WebMvcConfig.java
+++ demo/src/main/java/com/fasulting/config/WebMvcConfig.java
@@ -32,7 +32,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
         registry.addMapping("/**")
                 .allowedOrigins("*")
                 .allowedMethods("*")
-                .allowedHeaders("Authorization");
+                .allowedHeaders("*");
     }
 
 
