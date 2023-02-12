a3fdf9fbc1fb1a51cf950c7604c05ea9238f14cf
mirimy <mirimy97@gmail.com>
2023-02-12 20:25:22 +0900

    Feat: 나의활동 페이지 즐겨찾기 on/off 기능,api

--- fasulting/src/components/Card/FavoriteCard.js
+++ fasulting/src/components/Card/FavoriteCard.js
@@ -8,12 +8,42 @@ import { Grid, IconButton } from "@mui/material";
 import StarIcon from "@mui/icons-material/Star";
 import styles from "./FavResCard.module.css";
 import { useNavigate } from "react-router-dom";
+import axiosAPi from "../../api/axiosApi";
+import { useSelector } from "react-redux";
 
 function FavoriateCard({ fav }) {
   const navigate = useNavigate();
+  const userSeq = useSelector((state) => state.user.userSeq);
+  const [liked, setLiked] = React.useState(true);
+
   const moveDetail = () => {
     navigate(`/detail/${fav.psSeq}`);
   };
+
+  const disLike = (e) => {
+    e.stopPropagation();
+    if (!liked) {
+      axiosAPi
+        .post("/favorite", {
+          userSeq: userSeq,
+          psSeq: fav.psSeq,
+        })
+        .then((res) => console.log("좋아요", res))
+        .catch((e) => console.log("좋아요", e));
+    } else {
+      axiosAPi
+        .delete("/favorite", {
+          data: {
+            userSeq: userSeq,
+            psSeq: fav.psSeq,
+          },
+        })
+        .then((res) => console.log("좋아요취소", res))
+        .catch((e) => console.log("좋아요취소", e));
+    }
+    setLiked((current) => !current);
+  };
+
   return (
     <div className={styles.column}>
       <div className={styles.card} onClick={moveDetail}>
@@ -33,7 +63,19 @@ function FavoriateCard({ fav }) {
             </Typography>
           </IconButton>
           <IconButton color="error" aria-label="favorite">
-            <FavoriteIcon />
+            {liked ? (
+              <FavoriteIcon
+                fontSize="large"
+                sx={{ color: "#e64c3c" }}
+                onClick={disLike}
+              />
+            ) : (
+              <FavoriteIcon
+                fontSize="large"
+                sx={{ color: "#d9d4cf" }}
+                onClick={disLike}
+              />
+            )}
           </IconButton>
         </div>
       </div>
