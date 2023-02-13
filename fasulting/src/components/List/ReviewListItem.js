5832bae177c098914009d60b02df471a66c6d750
mirim <mrmy@kakao.com>
2023-02-13 17:19:41 +0900

    Fix: 병원측, 상담자측 리뷰 정보 다르게 표시

--- fasulting/src/components/List/ReviewListItem.js
+++ fasulting/src/components/List/ReviewListItem.js
@@ -6,28 +6,33 @@ import { useLocation } from "react-router-dom";
 import { useState } from "react";
 
 function ReviewListItem({ review }) {
-  // const [displayHospital, setDisplayHospital] = useState();
-  // const path = useLocation().pathname.slice(1, 7);
+  const [displayHospital, setDisplayHospital] = useState();
+  const path = useLocation().pathname.slice(1, 7);
   // 현재 path가 detail이거나 (병원)mypage 이면 병원이름 보여주지 않음
-  // useEffect(() => {
-  //   const temp = (path === "detail") | (path === "mypage") ? true : false;
+  useEffect(() => {
+    const temp = (path === "detail") | (path === "mypage") ? true : false;
 
-  //   setDisplayHospital(temp);
-  // }, []);
+    setDisplayHospital(temp);
+  }, []);
 
   return (
     <div>
       {/* 테스트이메일 문제 */}
-      <p className={styles.name}>{review.userEmail.charAt(0) + review.userEmail.charAt(1) + "***"}</p>
+      {displayHospital && displayHospital ? (
+        <p className={styles.name}>
+          {review.userEmail.charAt(0) + review.userEmail.charAt(1) + "***"}
+        </p>
+      ) : null}
+
       <div className={styles.oneLine}>
         <StarIcon sx={{ fontSize: 18, color: "#EECC51" }} />
         <p className={styles.rating}>{review.point.toFixed(1)}</p>
-        {/* {displayHospital && displayHospital ? null : ( */}
+        {displayHospital && displayHospital ? null : (
           <p className={styles.gray}>{review.psName}</p>
-        {/* )} */}
-        {/* {displayHospital && displayHospital ? null : ( */}
+        )}
+        {displayHospital && displayHospital ? null : (
           <p className={styles.gray}>|</p>
-        {/* )} */}
+        )}
         {review.subCategoryName.map((sub, index) => (
           <p key={index} className={styles.gray}>
             {sub}
@@ -40,6 +45,5 @@ function ReviewListItem({ review }) {
     </div>
   );
 }
-
 
 export default ReviewListItem;
