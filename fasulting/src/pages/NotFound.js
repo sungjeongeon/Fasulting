a80a744fad8d5de4c5e010bb99206cdf0d973a8f
mirim <mrmy@kakao.com>
2023-02-14 09:45:22 +0900

    Feat: 404 에러페이지 css

--- fasulting/src/pages/NotFound.js
+++ fasulting/src/pages/NotFound.js
@@ -1,9 +1,49 @@
-function NotFound () {
+import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
+import Link from "@mui/material/Link";
+
+function NotFound() {
   return (
-    <div style={{display: "flex", justifyContent: "center"}}>
-      <h1> 404 Not Found - CSS 필요</h1>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        position: "absolute",
+        top: "50%",
+        left: "50%",
+        transform: "translate(-50%, -50%)",
+      }}
+    >
+      <img
+        style={{
+          width: "25rem",
+        }}
+        src="/assets/images/notFound404.png"
+      />
+      <h2
+        style={{
+          margin: "0",
+        }}
+      >
+        페이지를 찾을 수 없어요 :(
+      </h2>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          margin: "0",
+          color: "#72a1a6",
+        }}
+      >
+        <ArrowBackIosIcon fontSize="small" />
+        <Link href="/">
+          <h5>메인페이지로 돌아가기</h5>
+        </Link>
+      </div>
     </div>
-  )
+  );
 }
 
-export default NotFound
+export default NotFound;
