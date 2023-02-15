7de8d817b5a38025525ad7ca2aafbb38413eb074
good-da22 <rnxogml3000@gmail.com>
2023-02-15 11:16:52 +0900

    fix : email code 수정

--- demo/src/main/java/com/fasulting/domain/email/controller/EmailController.java
+++ demo/src/main/java/com/fasulting/domain/email/controller/EmailController.java
@@ -39,9 +39,6 @@ public class EmailController {
 
         try {
             code = emailService.sendRegistCodeMessage(email);
-            session.setAttribute(email, code);
-            // 세션 유지시간 설정(초단위)
-            session.setMaxInactiveInterval(3*60); // 3분
 
         } catch (Exception e) {
 
@@ -50,7 +47,7 @@ public class EmailController {
         }
 
         log.info(LogCurrent.logCurrent(getClassName(), getMethodName(), END));
-        return ResponseEntity.status(200).body("success");
+        return ResponseEntity.status(200).body(ResponseBody.create(200, "success", code));
     }
 
     /**
@@ -70,38 +67,37 @@ public class EmailController {
 
         try {
             code = emailService.sendResetCodeMessage(email);
-            session.setAttribute(email, code);
-            // 세션 유지시간 설정(초단위)
-            session.setMaxInactiveInterval(3*60); // 3분
 
         } catch (Exception e) {
+
             log.info(LogCurrent.logCurrent(getClassName(), getMethodName(), END));
             return ResponseEntity.status(400).body(ResponseBody.create(400, "fail"));
         }
+
         log.info(LogCurrent.logCurrent(getClassName(), getMethodName(), END));
-        return ResponseEntity.status(200).body(ResponseBody.create(200, "success"));
+        return ResponseEntity.status(200).body(ResponseBody.create(200, "success", code));
     }
 
-    /**
-     * 이메일 인증 코드 확인 (accessCode, email)
-     * @return
-     */
-    @PostMapping("/access")
-    public ResponseEntity<?> checkEmailCode(@RequestBody EmailReqDto emailReqDto, HttpServletRequest request) {
-        // 이메일 인증 코드 & Server에서 전송한 이메일 코드 일치 여부 확인
-        // 인증코드 발송 시 인증코드를 담아서 전송 후 프론트단에서 일치 여부 확인
-
-        log.info(LogCurrent.logCurrent(getClassName(), getMethodName(), START));
-
-        HttpSession session = request.getSession();
-        String code = (String)session.getAttribute(emailReqDto.getEmail());
-
-        if(code.equals(emailReqDto.getEmailCode())){
-            session.removeAttribute(emailReqDto.getEmail());
-            log.info(LogCurrent.logCurrent(getClassName(), getMethodName(), END));
-            return ResponseEntity.status(200).body(ResponseBody.create(200, "success"));
-        }
-        log.info(LogCurrent.logCurrent(getClassName(), getMethodName(), END));
-        return ResponseEntity.status(400).body(ResponseBody.create(500, "fail"));
-    }
+//    /**
+//     * 이메일 인증 코드 확인 (accessCode, email)
+//     * @return
+//     */
+//    @PostMapping("/access")
+//    public ResponseEntity<?> checkEmailCode(@RequestBody EmailReqDto emailReqDto, HttpServletRequest request) {
+//        // 이메일 인증 코드 & Server에서 전송한 이메일 코드 일치 여부 확인
+//        // 인증코드 발송 시 인증코드를 담아서 전송 후 프론트단에서 일치 여부 확인
+//
+//        log.info(LogCurrent.logCurrent(getClassName(), getMethodName(), START));
+//
+//        HttpSession session = request.getSession();
+//        String code = (String)session.getAttribute(emailReqDto.getEmail());
+//
+//        if(code.equals(emailReqDto.getEmailCode())){
+//            session.removeAttribute(emailReqDto.getEmail());
+//            log.info(LogCurrent.logCurrent(getClassName(), getMethodName(), END));
+//            return ResponseEntity.status(200).body(ResponseBody.create(200, "success"));
+//        }
+//        log.info(LogCurrent.logCurrent(getClassName(), getMethodName(), END));
+//        return ResponseEntity.status(400).body(ResponseBody.create(500, "fail"));
+//    }
 }
